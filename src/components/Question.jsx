3ebe734e4c876f831981c9c5517b99703e722c36
Sebastian Oßner <github@sebastianoner.com>
2024-01-25 19:19:37 +0100

    Update CSS and fix some warnings

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,31 +1,30 @@
 import React, { useState } from 'react';
-import './Question.css'
 
 const Question = ({ data, onAnswerSelected }) => {
 
     const { mep_id, mep_name, correctAnswer, options } = data;
     const [showHint, setShowHint] = useState(false);
     const [answered, setAnswered] = useState(false);
+    const [disabled, setDisabled] = useState(false);
     const baseUrl = 'https://www.europarl.europa.eu/mepphoto/'; // Base URL
     const imageUrl = `${baseUrl}${mep_id}.jpg`; // Interpolated URL
-    const delay = ms => new Promise(
-        resolve => setTimeout(resolve, ms)
-    );
 
     return (
-        <div className="question-container">
-            <img src={imageUrl} height={400} width={340} />
-            <div className="options-container">
+        <div className="quiz-container">
+            <img src={imageUrl} className='question-image' alt='' />
+            <div className='options-container'>
                 {options.map((option, index) => (
                     <button
                         key={index}
                         className="option-button"
-                        style={{ backgroundColor: answered ? (option == correctAnswer ? '#00dd44' : '#ff0040') : '#007bff' }}
+                        disabled={disabled}
+                        style={{ backgroundColor: answered ? (option === correctAnswer ? '#467A39' : '#DA2131') : '#004494' }}
                         onClick={() => {
-                            setAnswered(true);setShowHint(false);
+                            setAnswered(true);setShowHint(false);setDisabled(true)
                             setTimeout(() => {
                                 onAnswerSelected(option);
                                 setAnswered(false);
+                                setDisabled(false)
                             }, 1000);
                         }}
                     >
